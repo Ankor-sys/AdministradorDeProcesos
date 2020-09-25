1f9fc337f3187a74616f534ef81bb5855c0e2a6d
Ashly2015 <ranbr200015@gmail.com>
2020-09-24 20:33:49 -0600

    Creacion de tabla

--- Administrador_Procesos/Administrador_Procesos/Form1.Designer.cs
+++ Administrador_Procesos/Administrador_Procesos/Form1.Designer.cs
@@ -28,12 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnVer = new System.Windows.Forms.Button();
             this.btnFinalizar = new System.Windows.Forms.Button();
             this.btnGrafica = new System.Windows.Forms.Button();
             this.lblContador = new System.Windows.Forms.Label();
+            this.dgvProceso = new System.Windows.Forms.DataGridView();
+            this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProceso)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -95,11 +105,74 @@
             this.lblContador.Text = "Numero de procesos: ";
             this.lblContador.Click += new System.EventHandler(this.label1_Click);
             // 
+            // dgvProceso
+            // 
+            this.dgvProceso.AllowUserToAddRows = false;
+            this.dgvProceso.AllowUserToDeleteRows = false;
+            this.dgvProceso.BackgroundColor = System.Drawing.Color.Teal;
+            this.dgvProceso.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProceso.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col1,
+            this.col2,
+            this.col3,
+            this.col4,
+            this.col5,
+            this.col6});
+            this.dgvProceso.Location = new System.Drawing.Point(15, 63);
+            this.dgvProceso.Name = "dgvProceso";
+            this.dgvProceso.ReadOnly = true;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Teal;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.PowderBlue;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvProceso.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvProceso.Size = new System.Drawing.Size(624, 393);
+            this.dgvProceso.TabIndex = 19;
+            // 
+            // col1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.col1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.col1.HeaderText = "# Proceso";
+            this.col1.Name = "col1";
+            this.col1.ReadOnly = true;
+            // 
+            // col2
+            // 
+            this.col2.HeaderText = "Proceso";
+            this.col2.Name = "col2";
+            this.col2.ReadOnly = true;
+            // 
+            // col3
+            // 
+            this.col3.HeaderText = "Prioridad del Proceso";
+            this.col3.Name = "col3";
+            this.col3.ReadOnly = true;
+            // 
+            // col4
+            // 
+            this.col4.HeaderText = "ID";
+            this.col4.Name = "col4";
+            this.col4.ReadOnly = true;
+            // 
+            // col5
+            // 
+            this.col5.HeaderText = "Memoria Fisica";
+            this.col5.Name = "col5";
+            this.col5.ReadOnly = true;
+            // 
+            // col6
+            // 
+            this.col6.HeaderText = "Memoria Virtual";
+            this.col6.Name = "col6";
+            this.col6.ReadOnly = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 464);
+            this.Controls.Add(this.dgvProceso);
             this.Controls.Add(this.panel1);
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -109,6 +182,7 @@
             this.TransparencyKey = System.Drawing.Color.LightCyan;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProceso)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -120,6 +194,13 @@
         private System.Windows.Forms.Label lblContador;
         private System.Windows.Forms.Button btnGrafica;
         private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.DataGridView dgvProceso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col6;
     }
 }
 
